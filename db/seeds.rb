17dcf0a9ee482a307d44784f0f15711806370eb9
navedrahim <nrah1988@gmail.com>
2021-08-21 21:53:30 -0400

    db seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Comment.destroy_all
+Project.destroy_all
+User.destroy_all
+
+
+naved = User.create!(username: "bobaloo142", password_digest: "scpswmd1")
+
+pp "#{User.count} users created"
+
+beer_me = Project.create!(name: "Beer Me", github_link: "https://github.com/navedrahim/beer-me", deployed_link: "https://brave-fermat-adf260.netlify.app", description: "Beer Me utilizes a third party API to provide info on breweries. Built with vanilla JavaScript, HTML, CSS.", screenshot: "https://i.imgur.com/lcaXk3h.jpg", user: naved)
+
+electrify = Project.create!(name: "Electrify", github_link: "https://github.com/navedrahim/electrify", deployed_link: "https://electrify.netlify.app/", description: "Electrify is a great place to research electric vehicles. API created using Airtable. Front end built with React", screenshot: "https://i.imgur.com/elj96vJ.jpg", user: naved)
+
+b_side_collective = Project.create!(name: "B-Side Collective", github_link: "https://github.com/navedrahim/b-side-collective", deployed_link: "https://b-side-collective.netlify.app/", description: "This app lets users browse vinyl records and make listings of their own. Built with MongoDB, Mongoose, Express, React", screenshot: "https://i.imgur.com/cBWfi6z.png", user: naved)
+
+pp "#{Project.count} projects created"
