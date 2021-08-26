a4d89074cd479dbe95ddf9cc1f738d5ce6fa4f64
navedrahim <nrah1988@gmail.com>
2021-08-26 14:55:00 -0400

    updated apiconfig

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const base =
   process.env.NODE_ENV === "production"
-    ? "deployed URL"
+    ? "https://nr-portfolio-api.herokuapp.com"
     : "http://localhost:3000";
 
 const api = axios.create({
